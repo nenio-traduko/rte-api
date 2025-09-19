f23c198baba13c51eb689ff0dc6fb9d9585ff525
David <david.alvarado@met.com>
2025-09-19 11:05:42 +0200

    Adds better error messaging to the balancing energy API endpoints and a better example.

--- example.py
+++ example.py
@@ -1,5 +1,7 @@
 from os import getenv
-from rte_api import WholesaleMarketAPI
+from rte_api import BalancingEnergyAPI, ContentType
+from datetime import datetime
+from zoneinfo import ZoneInfo
 from dotenv import load
 
 load()
@@ -7,6 +9,9 @@ load()
 client_id = getenv("RTE_CLIENT_ID", "")
 client_secret = getenv("RTE_CLIENT_SECRET", "")
 
-api = WholesaleMarketAPI(client_id, client_secret)
+start_date = datetime(year=2023, month=10, day=1, tzinfo=ZoneInfo("Europe/Paris"))
+end_date = datetime(year=2023, month=11, day=5, tzinfo=ZoneInfo("Europe/Paris"))
+api = BalancingEnergyAPI(client_id, client_secret)
 
-print(api.france_power_exchanges())
+data = api.imbalance_data(start_date, end_date, ContentType.CSV)
+print(data)
